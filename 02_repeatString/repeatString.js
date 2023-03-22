ada4d70c61e32035cf6d1e9e8518cca177d69b49
Jeremy Durden <jeremydurden@gmail.com>
2023-03-22 13:35:04 -0700

    first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    let value = "";
+    for (i = 0; i < num; i++) {
+      value += string;
+    }
+    return value;
+  }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const arr = string.split("");
+  const reversedArr = arr.reverse();
+  const newString = reversedArr.join("");
+  return newString;
 };
 
 // Do not edit below this line
