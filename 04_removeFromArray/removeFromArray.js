2bce3cc78e077a1f2c0c6c15bbab02e0ae275281
Jeremy Durden <jeremydurden@gmail.com>
2023-03-22 13:33:43 -0700

    first 3 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...theArguments) {
+  theArguments.forEach((arg) => {
+    const indexToRemove = arr.findIndex((num) => num === arg);
 
+    if (indexToRemove === -1) {
+      return;
+    } else {
+      arr.splice(indexToRemove, 1);
+    }
+  });
+  return arr;
 };
 
 // Do not edit below this line
