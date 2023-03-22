a8fde65dad30c636299b191202450e3d018eaefb
Jeremy Durden <jeremydurden@gmail.com>
2023-03-22 14:04:06 -0700

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (argA, argB) {
+  if (typeof argA !== "number" || typeof argB !== "number") {
+    return "ERROR";
+  }
+  if (argA < 0 || argB < 0) {
+    return "ERROR";
+  }
+  let smaller;
+  let larger;
+  if (argA < argB) {
+    smaller = argA;
+    larger = argB;
+  } else if (argB < argA) {
+    smaller = argB;
+    larger = argA;
+  }
+  const array = [];
+  for (i = smaller; i <= larger; i++) {
+    array.push(i);
+  }
+  const sum = array.reduce((acc, curr) => acc + curr);
+  return sum;
 };
 
 // Do not edit below this line
